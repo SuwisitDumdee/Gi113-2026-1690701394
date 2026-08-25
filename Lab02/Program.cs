b8f3c2b65aec72dd7ebefb3e56cb4a12ad9c6238
SuwisitDumdee <suwisitdumdee@gmail.com>
2026-08-25 20:58:47 +0700

    Added Part B code

--- Lab02/Program.cs
+++ Lab02/Program.cs
@@ -12,6 +12,7 @@ namespace Lab02
     {
         static void Main(string[] args)
         {
+            // Part A
             string bossName = "Kirin";
             char rank = 'S';
             int level = 7;
@@ -43,7 +44,62 @@ namespace Lab02
             hpPercent = currentHp * 100 / maxHp;
             Console.WriteLine($"HP Percent: {hpPercent}%");
 
+            // Part B
+            string minibossName = "AUNGPO";
+            char rank = 'S';
+            int level = 15;
+            int maxMana = 500;
+            int currentMana = 200;
 
+            Console.WriteLine($"MINIBOSS STATUS: INITIAL");
+            Console.WriteLine($"Name: {minibossName}");
+            Console.WriteLine($"Rank: {rank}");
+            Console.WriteLine($"Level: {level}");
+            Console.WriteLine($"Mana: {currentMana} / {maxMana}");
+            Console.WriteLine();
+
+            string playerName = "Numpan";
+            char rank = 'E';
+            int level = 5;
+            int maxStamina = 1000;
+            int currentStamina = 600;
+
+            Console.WriteLine($"PLAYER STATUS: INITIAL");
+            Console.WriteLine($"Name: {playerName}");
+            Console.WriteLine($"Rank: {rank}");
+            Console.WriteLine($"Level: {level}");
+            Console.WriteLine($"Stamina: {currentStamina} / {maxStamina}");
+            Console.WriteLine();
+
+            string playerName = "Kawin";
+            char rank = 'C';
+            int level = 7;
+            double criMultiplier = 0.75;
+
+            Console.WriteLine($"PLAYER STATUS: INITIAL");
+            Console.WriteLine($"Name: {playerName}");
+            Console.WriteLine($"Rank: {rank}");
+            Console.WriteLine($"Level: {level}");
+            Console.WriteLine($"Crit Multiplier: {criMultiplier}");
+            Console.WriteLine();
+
+            string npcName = "Jippo";
+            char rank = 'A';
+            int level = 20;
+            int maxHP = 2500;
+            float attackSpeed = 125.5f;
+            double critMultiplier = 2.35;
+            bool isNPC = true;
+
+            Console.WriteLine($"NPC STATUS: INITIAL");
+            Console.WriteLine($"Name: {npcName}");
+            Console.WriteLine($"Rank: {rank}");
+            Console.WriteLine($"Level: {level}");
+            Console.WriteLine($"Max HP: {maxHP}");
+            Console.WriteLine($"Attack Speed: {attackSpeed}");
+            Console.WriteLine($"Crit Multiplier: {critMultiplier}");
+            Console.WriteLine($"Is NPC: {isNPC}");
+            Console.WriteLine();
         }
     }
 }
