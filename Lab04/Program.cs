09786947cf3152b30723b52cb4e1a306d045b4d9
SuwisitDumdee <suwisitdumdee@gmail.com>
2026-09-07 15:12:26 +0700

    CreateLab04

---
+++ Lab04/Program.cs
@@ -0,0 +1,17 @@
+﻿/*
+* Student ID : 1690701394
+* Name       : Suwisit Dumdee
+* Section    : 129B
+* No.        : N/A
+* Course     : GI113 Computer Programming (GI)
+*/
+namespace Lab04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
