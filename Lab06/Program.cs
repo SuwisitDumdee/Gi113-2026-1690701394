7b91988c4b9eff797b07996b423735ee7115e4f7
SuwisitDumdee <suwisitdumdee@gmail.com>
2026-09-21 16:43:41 +0700

    1690701394

--- Lab06/Program.cs
+++ Lab06/Program.cs
@@ -10,56 +10,25 @@ namespace Lab06
     internal class Program
     {
         static void Main(string[] args)
-        {   
-            //Ex:
-            bool isPoisoned = false;
-            if (isPoisoned) { } // ได้เลยเพราะ isPoisoned = false
-            if (!isPoisoned) { } // ไม่จริง เพราะ isPoisoned = false
-
-            bool haskey = true;
-            Console.Write("Your level (1-99): ");
-            bool ok = int.TryParse(Console.ReadLine(), out int level);
-
-            if (!ok || level < 1 || level > 99)
-            {
-                Console.WriteLine("Invalid Level Input");
-            }
-            else if (level >= 10 && haskey)
-            {
-                Console.WriteLine("Boss floor unlocked.");
-            }
-            else if (level >= 5)
-            {
-                if (haskey == true)
-                {
-                    Console.WriteLine("The door opens.");
-                }
-                else
-                { 
-                    Console.WriteLine("Locked. Find a key");
-                }
-            }
-            else
-            {
-                Console.WriteLine("The door stays shut.");
-            }
-            //End Ex
-
+        {               
             int heroHp = 100;
             int monHp = 100;
             int heroAtk = 50;
             int potionHeal = 50;
+            int monAtk = 50;
+            bool shieldActive = false;
 
-            Console.WriteLine("HERO BRAIN ENCOUNTER A MONSTER....");
+            Console.WriteLine(">>> JIPPO SLAYER <<<");
             Console.WriteLine("ACTION 1: ATTACK");
-            Console.WriteLine("ACTION 2: DRINK HP POTION");
+            Console.WriteLine("ACTION 2: DEFENSE ATTACK");
+            Console.WriteLine("ACTION 3: DRINK HP POTION");
 
-            Console.Write("CHOOSE YOUR ACTION (1-2): ");
+            Console.Write("CHOOSE YOUR ACTION (1-3): ");
             bool isInputValid = int.TryParse(Console.ReadLine(), out int choice);
 
-            if (isInputValid == false || choice < 1 || choice > 2)
+            if (isInputValid == false || choice < 1 || choice > 3 )
             {
-                Console.WriteLine("Invalid Input, Please enter action between 1 and 2");
+                Console.WriteLine("Invalid Input, Please enter action between 1 and 3");
             }
             else if (choice == 1)
             {
@@ -74,6 +43,18 @@ namespace Lab06
                 }
             }
             else if (choice == 2)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Hero raised the shield!");
+                Console.WriteLine("Shield will reduce damage by 50%");
+
+                int reducedDamage = monAtk / 2;
+                heroHp -= reducedDamage;
+                Console.WriteLine($"Monster attacked!");
+                Console.WriteLine($"Shield blocked some damage");
+                Console.WriteLine($"Hero took {reducedDamage} DMG");
+            }
+            else if (choice == 3)
             {
                 heroHp += potionHeal;
                 Console.WriteLine($"Hero drank a potion, Hero HP is now {heroHp} Points");
