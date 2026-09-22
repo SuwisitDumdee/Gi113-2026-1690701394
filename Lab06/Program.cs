bbb8c6299a1d42eb011abceed1c2cafce7af9912
Suwisit <suwisitdumdee@gmail.com>
2026-09-22 23:11:58 +0700

    1690701394

--- Lab06/Program.cs
+++ Lab06/Program.cs
@@ -10,29 +10,31 @@ namespace Lab06
     internal class Program
     {
         static void Main(string[] args)
-        {               
+        {
             int heroHp = 100;
             int monHp = 100;
             int heroAtk = 50;
-            int potionHeal = 50;
+            int heroMana = 50;
+            int manaRestore = 30;
             int monAtk = 50;
             bool shieldActive = false;
 
             Console.WriteLine(">>> JIPPO SLAYER <<<");
             Console.WriteLine("ACTION 1: ATTACK");
             Console.WriteLine("ACTION 2: DEFENSE ATTACK");
-            Console.WriteLine("ACTION 3: DRINK HP POTION");
+            Console.WriteLine("ACTION 3: RESTORE MANA");
 
             Console.Write("CHOOSE YOUR ACTION (1-3): ");
             bool isInputValid = int.TryParse(Console.ReadLine(), out int choice);
 
-            if (isInputValid == false || choice < 1 || choice > 3 )
+            if (isInputValid == false || choice < 1 || choice > 3)
             {
                 Console.WriteLine("Invalid Input, Please enter action between 1 and 3");
             }
             else if (choice == 1)
             {
                 monHp -= heroAtk;
+
                 if (monHp <= 0)
                 {
                     Console.WriteLine($"Hero attacked the monster!!! with {heroAtk} DMG, Monster is defeated!");
@@ -50,14 +52,16 @@ namespace Lab06
 
                 int reducedDamage = monAtk / 2;
                 heroHp -= reducedDamage;
-                Console.WriteLine($"Monster attacked!");
-                Console.WriteLine($"Shield blocked some damage");
+
+                Console.WriteLine("Monster attacked!");
+                Console.WriteLine("Shield blocked some damage");
                 Console.WriteLine($"Hero took {reducedDamage} DMG");
             }
             else if (choice == 3)
             {
-                heroHp += potionHeal;
-                Console.WriteLine($"Hero drank a potion, Hero HP is now {heroHp} Points");
+                heroMana += manaRestore;
+
+                Console.WriteLine($"Hero restored {manaRestore} Mana, Hero Mana is now {heroMana} Points");
             }
 
 
